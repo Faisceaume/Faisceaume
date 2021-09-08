954369255587adfb21e0df3a3af94018024a06c5
C.B <cbanzet@gmail.com>
2021-09-08 15:38:57 +0200

    pour A

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -4,11 +4,12 @@ import { TasksListComponent,  } from './tasks-list/tasks-list.component';
 import { TasksRoutingModule } from './tasks-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { NewlinePipe } from './pipes/newline.pipe';
+import { TasksStatsComponent } from './tasks-stats/tasks-stats.component';
 
 
 
 @NgModule({
-  declarations: [TasksListComponent, NewlinePipe],
+  declarations: [TasksListComponent, NewlinePipe, TasksStatsComponent],
   imports: [
     SharedModule,
     CommonModule,
